83ae2315be38fd65c0825c12ec899aba89f6a34d
Krille med K <krillehamann@hotmail.com>
2023-08-09 19:07:13 +0200

    Added retries

--- rclone.sh
+++ rclone.sh
@@ -2,4 +2,4 @@
 
 worldpath=/home/krille/.config/unity3d/IronGate/Valheim/worlds_local
 
-rclone sync $worldpath gdrive:valheim-backup --include "*.db" --verbose
+rclone sync $worldpath gdrive:valheim-backup --include "*.db" --verbose --retries 3
